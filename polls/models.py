41836dd20f547be2c9d7651a2926f4497771ca95
Connie <2918155851@qq.com>
2018-10-16 19:33:41 +0800

    commit the third time

--- polls/models.py
+++ polls/models.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.db import models
-from django.utils import timezone
+#from django.utils import timezone
 import datetime
+
 
 # Create your models here.
 
@@ -21,3 +22,18 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+
+def was_published_recently(self):
+    now = timezone.now()
+    print now
+    return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+if __name__ == "__main__":
+    now = timezone.now()
+    print now
+
+
+
+
+
