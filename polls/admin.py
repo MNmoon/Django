142b3737b50feb1787a214334c0f3bce9c8c35dc
Connie <2918155851@qq.com>
2018-10-18 14:39:42 +0800

    8888

--- polls/admin.py
+++ polls/admin.py
@@ -1,14 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.contrib import admin
+from .models import Question,Choice
 
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
-from .models import Question
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_filter = ['pub_date']
 
-from .models import Choice
-
-admin.site.register(Choice)
+admin.site.register(Question, QuestionAdmin)
+search_fields = ['question_text']
